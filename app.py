6f87581725cb41ba86c2865050024ac0e762d2b2
iamaakashpal <aakashpal1198@gmail.com>
2022-10-15 03:02:04 +0530

    File Updated

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import sys
 from creditcarddefault.logger import logging
+from creditcarddefault.exception import CreditException
 app = Flask(__name__)
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("We are testing logged module")
+    try:
+        raise Exception(" !!! We are testing custom Exception !!! ")
+    except Exception as e:
+        creditcard = CreditException(e,sys)
+        logging.info(creditcard.error_message)
+        logging.info("We are testing logging module")
     return "CI CD Pipeline"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
