46b9315127d3b4741d57c23679da88489931e52f
iamaakashpal <aakashpal1198@gmail.com>
2022-10-15 20:17:56 +0530

    File Updated

--- creditcarddefault/entity/config_entity.py
+++ creditcarddefault/entity/config_entity.py
@@ -7,10 +7,9 @@ DataIngestionConfig=namedtuple("DataIngestionConfig",
 
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path","report_file_path","report_page_file_path"])
 
-# DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room",
-#                                                                    "transformed_train_dir",
-#                                                                    "transformed_test_dir",
-#                                                                    "preprocessed_object_file_path"])
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["transformed_train_dir",
+                                                                   "transformed_test_dir",
+                                                                   "preprocessed_object_file_path"])
 
 
 ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy","model_config_file_path"])
