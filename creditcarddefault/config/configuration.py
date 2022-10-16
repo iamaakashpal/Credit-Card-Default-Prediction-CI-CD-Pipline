f2bb2a9557794b971f1f9f6d7aca4eefbc9bdd08
iamaakashpal <aakashpal1198@gmail.com>
2022-10-16 12:07:44 +0530

    File Updated

--- creditcarddefault/config/configuration.py
+++ creditcarddefault/config/configuration.py
@@ -6,7 +6,7 @@ from creditcarddefault.exception import CreditException
 from creditcarddefault.util.util import read_yaml_file
 from creditcarddefault.constant import *
 
-class Configuartion:
+class Configuartion: # Visit example_configuration.ipynb file for Explaination
     def __init__(self,config_file_path:str = CONFIG_FILE_PATH,current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
         self.config_info = read_yaml_file(file_path = config_file_path)
         self.training_pipeline_config = self.get_training_pipeline_config()
@@ -32,7 +32,7 @@ class Configuartion:
 
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
         try:
-            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY] # training_pipeline_config: pipeline_name: creditcarddefault artifact_dir: artifact
             artifact_dir = os.path.join(ROOT_DIR,
             training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
             training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
